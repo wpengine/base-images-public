296908d4a52883b86bf5af7c2684472f000b22f5
JohnTunstall <jack.tunstall@wpengine.com>
2019-05-30 13:02:56 -0500

    adding rule exception for nested headers

--- linters/mdl/markdown/wpe_markdown.rb
+++ linters/mdl/markdown/wpe_markdown.rb
@@ -3,5 +3,8 @@ all
 # Set line length to 120 characters from 80.
 rule 'MD013', :line_length => 120
 
+# Allow headers to share names if separated by a larger header.
+rule 'MDO24', :allow_different_nesting => true
+
 # Set numbered lists to expect an increment in value.
 rule 'MD029', :style => "ordered"
